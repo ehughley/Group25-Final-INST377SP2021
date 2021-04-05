547b1032e2511b65a3682d0faddc701fe85591f8
bradleydayhoff <75460916+bradleydayhoff@users.noreply.github.com>
2021-04-05 19:34:57 -0400

    setting up API files for wars DB

--- models/index.js
+++ models/index.js
@@ -2,8 +2,28 @@ import DietaryRestrictions from './DietaryRestrictions.js';
 import DiningHall from './DiningHall.js';
 import Macros from './Macros.js';
 import Meals from './Meals.js';
+import countries from './countries.js';
+import country_sides from './country_sides.js';
+import locations from './locations.js';
+import outcomes from './outcomes.js';
+import participants from './participants.js';
+import sides from './sides.js';
+import war_outcomes from './war_outcomes.js';
+import wars from './wars.js';
+import war_sides from './war_sides.js';
+import where_fought from './where_fought.js';
 
 export default {
+  countries,
+  country_sides,
+  locations,
+  outcomes,
+  participants,
+  sides,
+  war_outcomes,
+  wars,
+  war_sides,
+  where_fought,
   DietaryRestrictions,
   DiningHall,
   Macros,
