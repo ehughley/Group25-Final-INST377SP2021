e584c4d96ba34ea04148f877aeacaa6c376b87b9
zahmad99 <zirwahzirwah98@gmail.com>
2021-04-06 15:56:56 -0400

    rename in files and index

--- models/index.js
+++ models/index.js
@@ -1,27 +1,23 @@
-import countries from './countries.js';
-import country_sides from './country_sides.js';
-import locations from './locations.js';
-import outcomes from './outcomes.js';
-import participants from './participants.js';
-import sides from './sides.js';
-import war_outcomes from './war_outcomes.js';
-import wars from './wars.js';
-import war_sides from './war_sides.js';
-import where_fought from './where_fought.js';
+import Countries from './countries.js';
+import CountrySides from './country_sides.js';
+import Locations from './locations.js';
+import Outcomes from './outcomes.js';
+import Participants from './participants.js';
+import Sides from './sides.js';
+import WarOutcomes from './war_outcomes.js';
+import Wars from './wars.js';
+import WarSides from './war_sides.js';
+import WhereFought from './where_fought.js';
 
 export default {
-  countries,
-  country_sides,
-  locations,
-  outcomes,
-  participants,
-  sides,
-  war_outcomes,
-  wars,
-  war_sides,
-  where_fought,
-  DietaryRestrictions,
-  DiningHall,
-  Macros,
-  Meals
+  Countries,
+  CountrySides,
+  Locations,
+  Outcomes,
+  Participants,
+  Sides,
+  WarOutcomes,
+  Wars,
+  WarSides,
+  WhereFought
 };
